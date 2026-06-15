18d9151a246f4fa6b4388e3e79321db37f322bcc
Fontaine Brahima <fontainebrahima@gmail.com>
2026-06-15 21:03:40 +0000

    update:input configution

---
+++ temp-tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#242a2b',
+      secondary: '#808080',
+      accent: {
+        DEFAULT: '#1cbccf',
+        secondary: '#18abbc',
+        tertiary: '#90c6cd',
+      },
+      grey: '#e8f0f1',
+    },
+  },
+  plugins: [],
+};
